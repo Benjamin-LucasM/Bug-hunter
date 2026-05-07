509bc177f7e8c7b34b99eb88f1dc16f379d35a9e
Benjamin <bemya001@osloskolen.no>
2026-05-07 12:00:49 +0200

    fix 4 & 5, fikset win.clear() ut av loop

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -171,9 +171,10 @@ def game_loop(win):
             food = spawn_food(snake)
             if speed > 0.05:
                 speed -= 0.002
-            win.clear()
         else:
             snake.pop()
+
+        win.clear()
 
         # Draw everything
         draw_border(win)
